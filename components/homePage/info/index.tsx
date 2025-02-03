87e709e0224ed9da217ecd32ce436c1370bf51a3
Mathan M <108012913+mathan44862@users.noreply.github.com>
2025-02-03 13:51:42 +0000

    info section issue changes

--- components/homePage/info/index.tsx
+++ components/homePage/info/index.tsx
@@ -1,7 +1,7 @@
 export default function InfoSection() {
   return (
     <div className="w-full min-h-screen bg-white text-black text-[40px] flex flex-col items-center justify-center px-4">
-      <p>Hi I'm Mathan M a passionate developer.</p>
+      <p>I&apos;m Mathan!</p>
       <p>
         Building sleek, scalable, and powerful applications with JavaScript,
         React, and more.
